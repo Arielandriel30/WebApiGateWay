302ac37614e25c7d24097ef2c478ee6ab131cae9
Arielandriel30 <ariel.landriel@hotmail.com>
2024-07-20 18:54:51 -0300

    jwtService

--- WebApiGateWay.Services/AuthorizationMiddlewareService.cs
+++ WebApiGateWay.Services/AuthorizationMiddlewareService.cs
@@ -1,39 +1,27 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using System;
-using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
-using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
 using WebApiGateWay.Entidades.Context;
-using Microsoft.Extensions.Configuration;// importamos para utilizar la conexion con settings json
+using Microsoft.Extensions.Configuration;
+using System.Security.Claims;
 
 namespace WebApiGateWay.Services
 {
     public interface IAuthorizationMiddlewareService
     {
         public Task InvokeAsync(HttpContext context);
-        
-
     }
+
     public class AuthorizationMiddlewareService : IAuthorizationMiddlewareService
     {
         private readonly RequestDelegate _next;
-        private readonly string _secretKey;
-        private readonly string _issuer;
-        private readonly string _audience;
-        private readonly UltimaMilla2Context _context; 
+        private readonly UltimaMilla2Context _context;
+        private readonly IJwtService _jwtService;
 
-        public AuthorizationMiddlewareService(RequestDelegate next, IConfiguration configuration, UltimaMilla2Context context)
+        public AuthorizationMiddlewareService(RequestDelegate next, IConfiguration configuration, UltimaMilla2Context context, IJwtService jwtService)
         {
             _next = next;
-            _secretKey = configuration["Jwt:SecretKey"];
-            _issuer = configuration["Jwt:Issuer"];
-            _audience = configuration["Jwt:Audience"];
             _context = context;
+            _jwtService = jwtService;
         }
 
         public async Task InvokeAsync(HttpContext context)
@@ -53,23 +41,10 @@ namespace WebApiGateWay.Services
             }
 
             var token = authorizationHeader.Substring("Bearer ".Length).Trim();
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var key = Encoding.ASCII.GetBytes(_secretKey);
-            var tokenValidationParameters = new TokenValidationParameters
-            {
-                ValidateIssuer = true,
-                ValidateAudience = true,
-                ValidateLifetime = true,
-                ValidateIssuerSigningKey = true,
-                ValidIssuer = _issuer,
-                ValidAudience = _audience,
-                IssuerSigningKey = new SymmetricSecurityKey(key),
-            };
 
             try
             {
-                var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out var validatedToken);
-
+                var principal = _jwtService.ValidateToken(token);
                 var claims = principal.Claims.ToDictionary(c => c.Type, c => c.Value);
 
                 if (!await ValidatePayloadAsync(claims, context))
